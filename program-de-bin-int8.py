935d946b7b8c57c70547bc2f4055fbd4d371ec7c
jay <103568936+jayyil@users.noreply.github.com>
2022-10-31 01:58:10 +0100

    Add files via upload

---
+++ program-de-bin-int8.py
@@ -0,0 +1,45 @@
+import pandas as pd 
+import numpy as np 
+import struct 
+import os 
+import argparse
+import time
+
+def decode(input_path,output_path):
+	decoded_items = []
+	size = os.path.getsize(input_path)
+	with open(input_path,'rb') as f:
+		for i in range(int(size/1)):
+			data = f.read(1)
+			decoded_items.append(struct.unpack('b',data))
+	with open(output_path,'w') as f:
+		for item in decoded_items:
+			f.write(str(item[0]))
+			f.write('\n')
+
+
+
+
+def main():
+
+	parser = argparse.ArgumentParser('Please enter the entire path of the file')
+
+	parser.add_argument('path', type=str, help='entire path of the file')
+
+	args = parser.parse_args()
+
+	path = args.path # get original csv file path from command line
+
+	decoded_file_path = path+'.csv' # generate decoded bin file path
+
+	
+	print('begin to decode')
+	start = time.time()
+	decoded_items = decode(path,decoded_file_path)
+	end = time.time()
+	print('done! decoded file has writte into {}'.format(decoded_file_path))
+	print('using time:', end-start)
+
+
+if __name__ == '__main__':
+	main()
---
+++ program-en-bin-int16.py
@@ -0,0 +1,38 @@
+import pandas as pd 
+import numpy as np 
+import struct 
+import os
+import argparse 
+import time
+
+def encode(input_path,output_path):
+	df = open(input_path,'r').read().splitlines()
+	df = [int(x) for x in df]
+	with open(output_path,'wb') as f:
+		for d in df:
+			bytes = struct.pack('h',d)
+			f.write(bytes)
+
+
+
+
+
+
+def main():
+	parser = argparse.ArgumentParser('Please enter the entire path of the file')
+	parser.add_argument('path', type=str, help='entire path of the file')
+	args = parser.parse_args()
+
+	path = args.path # get original csv file path from command line
+	encoded_file_path = path+'.bin' # generate encoded bin file path
+
+	print('begin to encode')
+	start = time.time()
+	encode(path,encoded_file_path)
+	end = time.time()
+	print('done! encoded file has writte into {}'.format(encoded_file_path))
+	print('using time:', end-start)
+
+
+if __name__ == '__main__':
+	main()
